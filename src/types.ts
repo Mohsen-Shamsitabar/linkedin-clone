0a5f11f344afb97b53ab5174d499977775661a2a
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2024-11-27 18:38:29 +0330

    feat(app): implement data types

---
+++ src/types.ts
@@ -0,0 +1,125 @@
+// === === === === === OPTIONS === === === === === //
+export type EmploymentType =
+  | "FULL_TIME"
+  | "PART_TIME"
+  | "SELF_EMPLOYED"
+  | "FREELANCE"
+  | "CONTRACT"
+  | "INTERNSHIP"
+  | "APPRENTICESHIP"
+  | "SEASONAL"
+  | "OTHER";
+
+export type WebsiteTypes =
+  | "PERSONAL"
+  | "COMPANY"
+  | "BLOG"
+  | "PORTFOLIO"
+  | "OTHER";
+
+export type FoundByType =
+  | "LINKEDIN"
+  | "COMPANY_WEBSITE"
+  | "INDEED"
+  | "RECRUITER"
+  | "OTHER";
+
+export type LocationType = "ON_SITE" | "HYBRID" | "REMOTE";
+
+export type PhoneType = "HOME" | "WORK" | "MOBILE";
+
+// === === === === === SUB TYPES === === === === === //
+
+export type ExperienceId = `EXP_${string}`;
+export type EducationId = `EDU_${string}`;
+export type UserId = `USER_${string}`;
+export type PostId = `POST_${string}`;
+export type CommentId = `CMNT_${string}`;
+
+export type Skill = string;
+
+export type Date = {
+  year: string;
+  month: string;
+};
+
+export type Website = {
+  url: string;
+  type: WebsiteTypes;
+};
+
+// === === === === === MAIN TYPES === === === === === //
+
+export type Experience = {
+  id: ExperienceId;
+  owner: UserId;
+  title: string;
+  active: boolean;
+  employmentType: EmploymentType;
+  company: string;
+  startDate: Date;
+  endDate: Date;
+  location: string;
+  locationType: LocationType;
+  description: string;
+  profileHeadline: string;
+  foundBy: FoundByType;
+  skills: Skill[];
+};
+
+export type Education = {
+  id: EducationId;
+  owner: UserId;
+  school: string;
+  degree: string;
+  fieldOfStudy: string;
+  startDate: Date;
+  endDate: Date;
+  grade: string;
+  description: string;
+  skills: Skill[];
+};
+
+export type Post = {
+  id: PostId;
+  owner: UserId;
+  caption: string;
+  media: string;
+  likedBy: UserId[];
+  comments: CommentId[];
+};
+
+export type Comment = {
+  id: CommentId;
+  writer: UserId;
+  postId: PostId;
+  text: string;
+  likedBy: UserId[];
+};
+
+export type ContactInfo = {
+  email: string;
+  phoneNumber: string;
+  phoneType: PhoneType;
+  address: string;
+  birthday: { month: string; day: string };
+  websites: Website[];
+};
+
+export type User = {
+  id: UserId;
+  firstName: string;
+  lastName: string;
+  avatar: string;
+  banner: string;
+  industary: string;
+  location: { country: string; city: string };
+  contactInfo: ContactInfo;
+  experiences: ExperienceId[];
+  educations: EducationId[];
+  posts: PostId[];
+  skills: Skill[];
+  connections: UserId[];
+  followers: UserId[];
+  followings: UserId[];
+};
