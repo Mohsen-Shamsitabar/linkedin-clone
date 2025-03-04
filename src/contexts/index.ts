cb270b3a4663f897c94e2c3c4e216988756061f3
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-03-04 13:20:02 +0330

    refactor export modules

--- src/contexts/index.ts
+++ src/contexts/index.ts
@@ -1,8 +1,14 @@
 export {
   Provider as UserProfileProvider,
   useContext as useUserProfile,
-  type ContextValue as ProfileUserData,
+  type ContextValue as UserProfileData,
 } from "./user-profile-context";
+
+export {
+  Provider as CompanyProfileProvider,
+  useContext as useCompanyProfile,
+  type ContextValue as CompanyProfileData,
+} from "./company-profile-context";
 
 export {
   Provider as LoggedUserProvider,
