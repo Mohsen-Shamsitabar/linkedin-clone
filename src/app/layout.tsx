d40b669a14c466b4efe1b58e7fffa39b6158118b
AmirManshadi <amirmanshadi.code@gmail.com>
2025-05-10 21:49:21 +0330

    refactor(app): modularize fonts used in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,10 @@
 import { ReactQueryProvider } from "@/providers";
 import type { LayoutPageProps } from "@/types";
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "normalize.css";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { cn } from "@/utility";
+import { geistMono, geistSans } from "./fonts";
 
 export const metadata: Metadata = {
   title: "Linkedin Clone",
@@ -27,7 +17,11 @@ export default function RootLayout(props: LayoutPageProps) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased w-screen overflow-x-hidden`}
+        className={cn(
+          "antialiased w-screen overflow-x-hidden",
+          geistSans.variable,
+          geistMono.variable,
+        )}
       >
         <ReactQueryProvider>
           <div className="w-full">{children}</div>
