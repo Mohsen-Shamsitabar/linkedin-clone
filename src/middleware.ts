ac344472afad8b7ec79dcfea4b51e2be53c81467
AmirManshadi <amirmanshadi.code@gmail.com>
2025-05-16 18:17:34 +0330

    feat(middleware): add stackMiddlewares function for middleware composition

---
+++ src/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse, type NextMiddleware } from "next/server";
+import { withProtectedRoutes } from "@/middlewares";
+
+const stackMiddlewares = (
+  middlewares: Array<(middleware: NextMiddleware) => NextMiddleware>,
+  index = 0,
+): NextMiddleware => {
+  const current = middlewares[index];
+
+  if (current) {
+    const next = stackMiddlewares(middlewares, index + 1);
+    return current(next);
+  }
+
+  return () => NextResponse.next();
+};
+
+export default stackMiddlewares([withProtectedRoutes]);
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|robots.txt).*)"],
+};
